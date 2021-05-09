d164ac9ae98a3c26409d36eb2a0246f0519e7dec
Admin <Admin@DESKTOP-3IU1GUE>
2021-05-09 11:33:47 +0530

    New Test Added

---
+++ OPAW5ApiProject/src/test/java/com/api/testScripts/SampleTest.java
@@ -0,0 +1,12 @@
+package com.api.testScripts;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void checkSample()
+	{
+		System.out.println("sample test running....");
+	}
+}
